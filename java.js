a1a66785de399c0021e0d2ef755d761b79e2cb49
SharpLad <143443228+yrezvani@users.noreply.github.com>
2023-09-20 13:38:32 +0100

    Add create grid functionality remove an extra function add create grid function

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('#container');
 const btn = document.querySelector('#button');
+let gridSize
 
 
 for (let i = 0; i <16; i++) {
@@ -13,14 +14,31 @@ for (let i = 0; i <16; i++) {
     container.appendChild(column);
 }
 
-let gridSize;
-
 function createGrid (gridSize) {
-    container.replaceChildren();
-    
-    gridSize = prompt ('Enter your grid si\e');
-
+    for (let i = 0; i < gridSize; i++) {
+        column = document.createElement('div');
+        column.classList.add('columns');
+        for (let j = 0; j < gridSize; j++) {
+            row = document.createElement('div');
+            row.classList.add('rows')
+            column.appendChild(row);
+        }
+        container.appendChild(column);
+    }    
 }
+
+function resetGame (gridSize) {
+    container.replaceChildren();
+    gridSize = prompt ('Enter grid size');
+    if (typeof gridSize == 'number') {
+        gridSize = prompt ('Oops! That\'s not a number. Enter a number for your grid size');
+    } else if (gridSize > 100) {
+        gridSize = prompt ('That\'s too big! Enter a grid size between 1 and 100');
+    }
+    createGrid (gridSize);
+}
+
+
 function changeColor () {
     column.style.backgroundColor = ('blue');
 }
@@ -31,11 +49,7 @@ container.addEventListener('mouseover', function(e) {
     }
 });
 
-function alert () {
-    alert('lskdjf');
-}
-
-btn.addEventListener('click', createGrid);
+btn.addEventListener('click', resetGame);
 
 
 
